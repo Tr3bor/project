bd157071a98fb061693f1f94096036b8f9de3a3c
Art Bekolli <abekolli57@gmail.com>
2024-02-03 03:08:38 +0100

    responsive

---
+++ js/about-us.js
@@ -0,0 +1,20 @@
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
